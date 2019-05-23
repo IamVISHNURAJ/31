f2e1ba22624e83d404d0ffb97695f921b23e7aa9
IamVISHNURAJ <50854010+IamVISHNURAJ@users.noreply.github.com>
2019-05-23 20:56:47 +0530

    Add files via upload

---
+++ 31.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    char a[100];
+    int i,flag=0;
+    gets(a);
+    for(i=0;a[i]!='\0';i++)
+    {
+    if(s[i]!=' ')
+    {
+        flagt++;
+    }
+   
+    }
+   printf("%d",flag);
+   return 0;
+}
